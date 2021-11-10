7e148a601cbdcbf7c0758ce1e299014631c20a86
ima18h <imadha@math.uio.no>
2021-11-11 00:32:50 +0100

    Fix constructor type checking logic

--- chaos_game.py
+++ chaos_game.py
@@ -4,31 +4,36 @@ import matplotlib.pyplot as plt
 
 class ChaosGame:
     def __init__(self, n: int = 3, r: float = 0.5):
-        """Need to make sure input is of right type, and 0 < r > 1, n > 2.
+        """Need to make sure input is of right type, and 0 < r < 1, n > 2.
         This checks first, then sets the attributes after. sets to default if not possible. """
         try:
             if not isinstance(n, int):
-                raise TypeError("n should be an int > 2")
+                raise TypeError("n should be an int")
         except TypeError as terr:
             print(terr.args)
             print("trying to convert")
             n = int(n)
-            if n < -2:
-                self.n = self.n * -1
-            elif -2 <= n <= 2:
-                print("using default n, 3")
-                self.n = 3
+        if n < -2:
+            print("n need to be int > 2, converting to positive int")
+            self.n = self.n * -1
+        elif -2 <= n <= 2:
+            print("using default n = 3")
+            self.n = 3
+        else:
+            self.n = n
+
         try:
-            if not isinstance(n, float):
-                raise TypeError("r should be an float 0 < r > 1")
+            if not isinstance(r, float):
+                raise TypeError("r should be a float 0 < r < 1")
         except TypeError as terr:
             print(terr.args)
             print("trying to convert")
             r = float(r)
-            if not (0 < r > 1):
-                print("using default, 0.5")
-                self.r = 0.5
-            else:
-                self.r = r
+        if not (0 < r < 1):
+            print("using default = 0.5")
+            self.r = 0.5
+        else:
+            self.r = r
+
 
 game = ChaosGame()
