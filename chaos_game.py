930a0237be33c235bb795bb28f1b7016c341c8e2
ima18h <ima18h@gmail.com>
2021-11-11 20:05:28 +0100

    Implement _generate_ngon() for problem 2b

--- chaos_game.py
+++ chaos_game.py
@@ -2,10 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
 class ChaosGame:
     def __init__(self, n: int = 3, r: float = 0.5):
         """Need to make sure input is of right type, and 0 < r < 1, n > 2.
         This checks first, then sets the attributes after. sets to default if not possible. """
+        self._n: int
+        self._r: float
+        self._corners: list
+
         try:
             if not isinstance(n, int):
                 raise TypeError("n should be an int")
@@ -37,8 +42,15 @@ class ChaosGame:
         else:
             self._r = r
 
+        self._generate_ngon()
+
+    def _generate_ngon(self):
+        theta = 2*np.pi / self._n
+        corners = [None] * self._n
+        for i in range(self._n):
+            corners[i] = (np.sin(theta*i), np.cos(theta*i))
+        self._corners = corners
 
 
-
-game = ChaosGame(33, 23)
-print(game._n, game._r)
+game = ChaosGame(5, 0.6)
+print(game._corners[3])
