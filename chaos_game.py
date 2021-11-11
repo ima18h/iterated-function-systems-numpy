7f1c88e451ce3e4c8ba196d9312841812e7b8233
ima18h <imadha@math.uio.no>
2021-11-11 19:23:06 +0100

    Fix some print messages

--- chaos_game.py
+++ chaos_game.py
@@ -17,6 +17,7 @@ class ChaosGame:
             print("n need to be int > 2, converting to positive int")
             self.n = self.n * -1
         elif -2 <= n <= 2:
+            print("n has to be int > 2")
             print("using default n = 3")
             self.n = 3
         else:
@@ -24,16 +25,18 @@ class ChaosGame:
 
         try:
             if not isinstance(r, float):
-                raise TypeError("r should be a float 0 < r < 1")
+                raise TypeError("TypeError: r should be a float 0 < r < 1")
         except TypeError as terr:
             print(terr.args)
             print("trying to convert")
             r = float(r)
         if not (0 < r < 1):
-            print("using default = 0.5")
+            print("r should be a float 0 < r < 1")
+            print("using default r = 0.5")
             self.r = 0.5
         else:
             self.r = r
 
 
-game = ChaosGame()
+game = ChaosGame(33, 23)
+print(game.n, game.r)
